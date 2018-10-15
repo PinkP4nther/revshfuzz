40d5299349012b46f515af9816209a0438809233
Pinky <36157015+PinkP4nther@users.noreply.github.com>
2018-10-15 13:50:23 +0000

    Update pd.php

--- pd.php
+++ pd.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Change 127.0.0.1 to your attacking machines IP then upload this file
+ * Add your attacking machines IP then upload this file
  * to the target webserver
  */
 $fd = fsockopen("IP TO CONNECT BACK TO",(int)$_GET['p']);if(!$fd){fclose($fd);}else{fclose($fd);}?>
